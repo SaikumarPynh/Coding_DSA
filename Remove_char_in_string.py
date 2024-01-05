c346313524f3762c01f1d67ea4a46945b6dade9c
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-05 19:32:54 +0530

    python code to remove a letter in a string

---
+++ Remove_char_in_string.py
@@ -0,0 +1,32 @@
+# to remove a perticular charecter in a string using replace built in method
+
+
+# test_str = input("entr the test string")
+# rem_char = input("enter the charecter to be removed")
+# new_str = test_str.replace(rem_char,"")
+# print(new_str)
+
+# to replace a charecter in a perticular position in a string
+
+# test_str = input("entr the test string")
+# rem_pos = input("enter the charecter  pos to be removed")
+# pos = test_str.find(rem_pos)
+# new_str = test_str.replace(test_str[pos],"")
+# print(new_str)
+
+# removing an char in a perticular position using native method 
+# test_str = input("entr the test string")
+# rem_pos = int(input("enter the charecter  pos to be removed"))
+# new_str = ''
+# for  i in test_str:
+#     if i != test_str[rem_pos]:
+#         new_str = new_str + i
+# print(new_str)
+str = input("enter the string:")
+letter = input("enter the letter to be removed:")
+
+new = ''
+for i in str:
+    if i != letter:
+        new += i
+print(new)
