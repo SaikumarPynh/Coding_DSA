0f32ffb73d2c2fddbb26a094ac2583d81987a7e7
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-05 19:12:06 +0530

    python code to find the maximum and minimum occurance of a letter in a another way

--- max_occr_char_in_string.py
+++ max_occr_char_in_string.py
@@ -1,10 +1,20 @@
 strings = input("enter the string")
+# dic = {}
+# for i in string:
+#     if i in dic.keys():
+#         dic[i] += 1
+#     else:
+#         dic[i] = 1
+# resmax = max(dic,key = dic.get)
+# resmin = min(dic,key = dic.get)
+# print(resmax,resmin)
 dic = {}
-for i in string:
-    if i in dic.keys():
+for i in strings:
+    if i in dic:
         dic[i] += 1
     else:
         dic[i] = 1
-resmax = max(dic,key = dic.get)
-resmin = min(dic,key = dic.get)
-print(resmax,resmin)
+print(dic)
+max_letter  =  max(dic, key = dic.get)
+min_letter = min(dic, key = dic.get)
+print("maximun is",max_letter, "minimun is",min_letter)
