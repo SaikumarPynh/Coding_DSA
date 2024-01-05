fe93a14b24453ea3637ca6c9626e6a4980b8ccad
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-05 19:05:07 +0530

    python code to accept string count of ovels

---
+++ accept_str_cont_all_ovels.py
@@ -0,0 +1,12 @@
+# str = input("enter the string")
+# ovels = ['a','e','i','o','u']
+# count = 0
+# for i in ovels:
+#     if i not in str:
+#         count += 1
+# if count > 0:
+#     print("not contains all the integers")
+
+# else:
+#     print("contains all ovels")
+
