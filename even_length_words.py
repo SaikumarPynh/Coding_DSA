2bc9fb7bafc78eb1051ad6e21f2a786bc755aca4
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-07 13:07:40 +0530

    python code on counting match charecter in the two strings

---
+++ even_length_words.py
@@ -0,0 +1,30 @@
+# test_str = input("entr the test string")
+# new = ''
+# l = test_str.split()
+# for i in l:
+#     if len(i) % 2 == 0:
+#         new = new + i + ' '
+# print(new)
+
+
+
+
+# test_str  = input("entr the test string")
+# new = []
+# l = test_str.split()
+# for i in l:
+#     if len(i) % 2 == 0:
+#         new.append(i)
+# print(' '.join(new))
+
+str = input("enter the string:")
+l = str.split()
+if len(l) % 2 == 0:
+    print("string having even length:")
+else:
+    print("string having odd length:")
+for i in l:
+    str = "".join(i)
+print(str)
+
+
