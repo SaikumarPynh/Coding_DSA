adfa45454a86f0bb139db2dc560dd25ccfe147dc
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-07 13:09:12 +0530

    python code on finding max and min charecter in a string

---
+++ max&min_occr_char_in_string.py
@@ -0,0 +1,23 @@
+# string = input("enter the string")
+# dic = {}
+# for i in string:
+#     if i in dic.keys():
+#         dic[i] += 1
+#     else:
+#         dic[i] = 1
+# resmax = max(dic,key = dic.get)
+# resmin = min(dic,key = dic.get)
+# print(resmax,resmin)
+str = input("enter the string:")
+lis = {}
+count = 0
+for i in str:
+    if i in lis:
+        lis[i] += 1
+    else:
+        lis[i] = 1
+print(lis)
+max_key = max(lis, key = lis.get)
+print(max_key)
+min_key = min(lis, key = lis.get)
+print(min_key)
