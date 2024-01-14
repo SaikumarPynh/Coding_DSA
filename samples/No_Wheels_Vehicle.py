671df6b98b05fc5486e7f8a7af3d81d0e252d0c0
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-14 16:11:34 +0530

    python code for finding wheels and vehicle

--- samples/No_Wheels_Vehicle.py
+++ samples/No_Wheels_Vehicle.py
@@ -1,23 +1,5 @@
-"""Problem Statement – An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to the given data below:
-
-1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
-2nd data, Total number of wheels = W
-The task is to find how many two-wheelers as well as four-wheelers need to manufacture as per the given data.
-Example :
-
-Input :
-200  -> Value of V
-540   -> Value of W
-
-Output :
-TW =130 FW=70
-
-Explanation:
-130+70 = 200 vehicles
-(70*4)+(130*2)= 540 wheels"""
-
-def find_vehicle(v,w):
-     # Equation 1: V = TW + FW
+def find_number_of_vehicles(V, W):
+    # Equation 1: V = TW + FW
     # Equation 2: W = 2 * TW + 4 * FW
 
     # Rearrange Equation 1 to get TW in terms of FW
@@ -29,15 +11,16 @@ def find_vehicle(v,w):
     # 2V - W = 2 * FW
     # FW = (2V - W) / 2
 
-    fw = ((2 * v) - w) // 2
-    tw = v - fw
-    return f"total two wheelers are{tw} and four wheelers are {fw}"
+    FW = (2 * V - W) // 2
+    TW = V - FW
 
+    return TW, FW
 
+# Example usage with the provided input
+V = 200  # Total number of vehicles
+W = 540  # Total number of wheels
 
+TW, FW = find_number_of_vehicles(V, W)
 
-
-
-v = int(input("enter the number of vehicles:"))
-w = int(input("enter the number of wheels:"))
-print(find_vehicle(v,w))
+# Output the results
+print(f"TW = {TW}, FW = {FW}")
