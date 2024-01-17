7c9d939f39c68ee0f9288076913091bb4164902c
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-17 23:32:03 +0530

    added some comments

--- even_length_words.py
+++ even_length_words.py
@@ -1,26 +1,26 @@
-# test_str = input("entr the test string")
-# new = ''
-# l = test_str.split()
-# for i in l:
-#     if len(i) % 2 == 0:
-#         new = new + i + ' '
-# print(new)
-# test_str  = input("entr the test string")
-# new = []
-# l = test_str.split()
-# for i in l:
-#     if len(i) % 2 == 0:
-#         new.append(i)
-# print(' '.join(new))
-#another way
-str = input("enter the string:")
-l = str.split()
-if len(l) % 2 == 0:
-    print("string having even length:")
-else:
-    print("string having odd length:")
+test_str = input("entr the test string")
+new = ''
+l = test_str.split()
 for i in l:
-    str = "".join(i)
-print(str)
+    if len(i) % 2 == 0:
+        new = new + i + ' '
+print(new)
+test_str  = input("entr the test string")
+new = []
+l = test_str.split()
+for i in l:
+    if len(i) % 2 == 0:
+        new.append(i)
+print(' '.join(new))
+#another way
+# str = input("enter the string:")
+# l = str.split()
+# if len(l) % 2 == 0:
+#     print("string having even length:")
+# else:
+#     print("string having odd length:")
+# for i in l:
+#     str = "".join(i)
+# print(str)
 
 
