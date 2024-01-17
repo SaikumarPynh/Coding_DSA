286f4d269028c1a5e0b5a54bc9cea0e4c90287ef
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-17 23:31:32 +0530

    added some comments

--- even_length_words.py
+++ even_length_words.py
@@ -5,10 +5,6 @@
 #     if len(i) % 2 == 0:
 #         new = new + i + ' '
 # print(new)
-
-
-
-
 # test_str  = input("entr the test string")
 # new = []
 # l = test_str.split()
@@ -16,7 +12,7 @@
 #     if len(i) % 2 == 0:
 #         new.append(i)
 # print(' '.join(new))
-
+#another way
 str = input("enter the string:")
 l = str.split()
 if len(l) % 2 == 0:
