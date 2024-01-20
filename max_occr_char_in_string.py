bbe27977405fe3170ba38dd3072fd6b5fe22d220
SaikumarPynh <saikumarpynh@gmail.com>
2024-01-20 21:10:26 +0530

    added some comments

--- max_occr_char_in_string.py
+++ max_occr_char_in_string.py
@@ -1,13 +1,15 @@
 strings = input("enter the string")
-"""# dic = {}
-# for i in string:
-#     if i in dic.keys():
-#         dic[i] += 1
-#     else:
-#         dic[i] = 1
-# resmax = max(dic,key = dic.get)
-# resmin = min(dic,key = dic.get)
-# print(resmax,resmin)"""
+"""
+dic = {}
+for i in string:
+    if i in dic.keys():
+        dic[i] += 1
+    else:
+        dic[i] = 1
+resmax = max(dic,key = dic.get)
+resmin = min(dic,key = dic.get)
+print(resmax,resmin)
+"""
 dic = {}
 for i in strings:
     if i in dic:
