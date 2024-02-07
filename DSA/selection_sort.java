372cb9d3dba02dae69db2246d02795d745932bb3
SaikumarPynh <saikumarpynh@gmail.com>
2024-02-07 21:27:51 +0530

    selection sort algortithm in java with some statements added

--- DSA/selection_sort.java
+++ DSA/selection_sort.java
@@ -1,26 +1,34 @@
+//we find minimum index in each iteration and replace the corresponding value with that ith value;
+
 public class selection_sort{
     public static int[] sort(int[] arr){
-
         for(int i = 0;i<arr.length-1;i++){
             int min = i;
             for(int j = i+1;j<arr.length;j++){
-              if(arr[min] > arr[j]) {
-                min = j;
-              }
+                if(arr[min] > arr[j]){
+                    min = j;
                 }
-                int temp = arr[min];
-                arr[min] = arr[i];
-                arr[i] = temp;
             }
-            return arr;
-
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
-    
+        return arr;
+    }
     public static void main(String[] args){
-        int[] arr = {1,46, 24, 52, 20, 9};
-        int[] sol = sort(arr);
-        for(int i:sol){
-            System.out.println(i);
+        int[] arr = {1, 46, 24, 52, 20, 9};
+        System.out.println("array before sorting is:");
+        for(int i:arr){
+            System.out.print(i+" ");
         }
+        System.out.println();
+
+        System.out.println("array after sorting is:");
+        int[] res =sort(arr);
+        for(int i:res){
+        System.out.print(i+" ");
+        }
+
+        
     }
 }
