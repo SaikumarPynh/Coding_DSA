6dd9d4e45b23691adb71a591ab222e244f3bf61d
SaikumarPynh <saikumarpynh@gmail.com>
2024-02-11 19:21:18 +0530

    finding the leders in an array using java

---
+++ DSA/leetcode/finding_leader_method2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class solution{
+    public static void result(int[] arr){
+   ArrayList<Integer> array = new ArrayList<>();
+   int n = arr.length;
+   int max = arr[n-1];
+   array.add(max);
+   for(int i=n-2;i>-1;i--){
+    if(arr[i]> max){
+        array.add(arr[i]);
+        max = arr[i];
+    }
+   }
+   System.out.print(array);
+    }
+    public static void main(String[] args){
+        int[] arr = {10, 22, 12, 3, 0, 6};
+        result(arr);
+    }
+}
