30247ddfb5777c14056ec1c5fbac8d08d2b53dde
SaikumarPynh <saikumarpynh@gmail.com>
2024-03-19 00:16:30 +0530

    finding the longest common prefix  some test case code added

--- DSA/leetcode/longest_common_prefix.java
+++ DSA/leetcode/longest_common_prefix.java
@@ -3,10 +3,16 @@ class Solution {
         if (strs == null || strs.length == 0) {
             return "";
         }
+        String shortestString = strs[0];
+        for (String str : strs) {
+            if (str.length() < shortestString.length()) {
+                shortestString = str;
+            }
+        }
         int count  = 0;
         boolean val = true;
         StringBuilder s = new StringBuilder();
-        while(val && count < strs[0].length()){
+        while(val && count < shortestString.length()){
             for(int i=1;i<strs.length;i++){
                 if(strs[i].charAt(count) != strs[0].charAt(count)){
                     val = false;
@@ -21,6 +27,7 @@ class Solution {
 
         return s.toString();
     }
+}
     public static void main(String[] args) {
         Solution obj = new Solution();
         String[] strs = {"flower","flow","floght"};
