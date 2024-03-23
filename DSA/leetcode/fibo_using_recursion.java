57671b7a9c6ae0a1e1f3a169a32ca1e218dff004
SaikumarPynh <saikumarpynh@gmail.com>
2024-03-23 22:15:36 +0530

    finding the fibonacci serires using recusrsion  findding series upto n terms

--- DSA/leetcode/fibo_using_recursion.java
+++ DSA/leetcode/fibo_using_recursion.java
@@ -11,6 +11,9 @@ public class fibo_using_recursion{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        System.out.println(num+"th term is "+fibo_nth_term(num));
+        System.out.println("fibo series upto "+num+"th term is");
+
         for(int i = 0;i<num;i++){
             System.out.print(fibo_nth_term(i)+" ");
         }
