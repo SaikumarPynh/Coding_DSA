94e1d78a1edc70713bc95a824a3fcc98ecacdff0
SaikumarPynh <saikumarpynh@gmail.com>
2024-03-27 00:27:06 +0530

    Reversing stack using LL

---
+++ DSA/leetcode/Reversing_LL.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+class ListNode{
+    int val;
+    ListNode next;
+    public ListNode(int val){
+        this.val = val;
+    }
+    public ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Reversing_LL{
+    static Stack<ListNode> s = new Stack<>();
+    static ListNode ReverseLinkedList(ListNode present){
+        while(present != null){
+            s.push(present);
+            present = present.next;
+        }
+        ListNode res = s.pop();
+        ListNode head = res;
+        while(!s.isEmpty()){
+            head.next = s.pop();
+            head = head.next;
+        }
+        head.next = null;
+        return res;
+    }
+    public static void main(String[] args){
+        ListNode head = new ListNode(10);
+        head.next = new ListNode(20);
+        head.next.next = new ListNode(30);
+        head.next.next.next = new ListNode(40);
+        head = ReverseLinkedList(head);
+        ListNode dummy = head;
+        while(dummy != null){
+            System.out.print(dummy.val+"->");
+            dummy = dummy.next;
+        }
+        System.out.println("null");
+
+    }
+
+}
