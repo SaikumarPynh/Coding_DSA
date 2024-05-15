a501757a87fb5a9a1fa9dc2a25855fca55afe451
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-15 15:15:14 +0530

    reverse star pyramid using java

---
+++ Patterns/Rev_pyramid.java
@@ -0,0 +1,15 @@
+public class Rev_pyramid {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+        for(int row = 0;row<n;row++){
+            for(int space = 0;space < row ; space++ ){
+                System.out.print(" ");
+            }
+            for(int star  = 2*(n-row);star> 1;star--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
