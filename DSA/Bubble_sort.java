6592850f71cd3c06d1d27ab3f71caa67084bf4f7
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-18 12:25:13 +0530

    bubble sorting algorithm code from the gg is added

--- DSA/Bubble_sort.java
+++ DSA/Bubble_sort.java
@@ -28,3 +28,27 @@ public class Bubble_sort{
         
     }
 }
+// class Solution
+// {
+//     //Function to sort the array using bubble sort algorithm.
+// 	public static void bubbleSort(int arr[], int n)
+//     {
+//         //code here
+//         for(int i = 0;i<n-1;i++){
+//             boolean swap = false;
+//             for(int j = 0;j<n-i-1;j++){
+//                 if(arr[j] > arr[j+1]){
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+//                     swap = true;
+//                 }
+                
+//             }
+//             if(!swap){
+//             break;
+//         }
+//         }
+        
+//     }
+// }
