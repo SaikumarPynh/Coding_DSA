f0f7226a97ba2139a49e771094cd280a8d027d67
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-25 18:40:04 +0530

    finding the  indexes of the array which gives the elements target element code adding with hashmap

--- DSA/leetcode/TwoSum.java
+++ DSA/leetcode/TwoSum.java
@@ -1,15 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int m = 0;
-        int n = 0;
-        for(int i = 0;i<nums.length - 1;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if((nums[i] + nums[j]) == target){
-                    m = i;
-                    n = j;
-                }
+        // int m = 0;
+        // int n = 0;
+        // for(int i = 0;i<nums.length - 1;i++){
+        //     for(int j = i+1;j<nums.length;j++){
+        //         if((nums[i] + nums[j]) == target){
+        //             m = i;
+        //             n = j;
+        //         }
+        //     }
+        // }
+        // return new int[] {m,n};
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i< nums.length;i++){
+            int ele = target - nums[i];
+            if(map.containsKey(ele)){
+                return new int[] {i,map.get(ele)};
             }
+            map.put(nums[i],i);
         }
-        return new int[] {m,n} ;
+        return new int[]{};
     }
 }
