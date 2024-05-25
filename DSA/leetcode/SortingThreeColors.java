7f3b3125499e630d80633d8e4a214e884d911571
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-25 19:41:36 +0530

    sorting three colors

---
+++ DSA/leetcode/SortingThreeColors.java
@@ -0,0 +1,17 @@
+class SortingThreeColors {
+    public void sortColors(int[] nums) {
+        for(int i = 0;i<nums.length - 1;i++){
+            int min = i;
+            int j = i;
+            while( j < nums.length){
+                if(nums[min] > nums[j]){
+                    min = j;
+                }
+                j++;
+            }
+            int temp = nums[min];
+            nums[min] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
