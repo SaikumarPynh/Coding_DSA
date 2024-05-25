32a10a29b12ad68b0721fcb58da2a6f4ae718f64
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-25 22:42:48 +0530

    finding the mejarity element in a array that repeats  more than len by 2 times

---
+++ DSA/leetcode/MejarityElement.java
@@ -0,0 +1,20 @@
+
+class  MejarityElement{
+    public int majorityElement(int[] nums) {
+        int len = nums.length / 2;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int ele:nums){
+            if(map.containsKey(ele)){
+                map.put(ele,map.get(ele)+1);
+            }else{
+                map.put(ele,1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()> len ){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
