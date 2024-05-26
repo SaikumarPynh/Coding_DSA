31db56fcc9de61d4676c877d14a86ba26dfc86b8
SaikumarPynh <saikumarpynh@gmail.com>
2024-05-26 22:50:21 +0530

    buying and selling the stock by making profit

--- DSA/leetcode/BuySellStock.java
+++ DSA/leetcode/BuySellStock.java
@@ -25,3 +25,18 @@ class Solution {
 //       return profit;
 //     }
 // }
+//class Solution {
+//     public int maxProfit(int[] prices) {
+//         int min = Integer.MAX_VALUE;
+//         int maxprof = 0;
+//         for(int i = 0;i < prices.length;i++){
+//             maxprof = Math.max(maxprof,prices[i] - min);
+//             min = Math.min(min, prices[i]);
+//         }
+
+
+
+
+//         return maxprof;
+//     }
+// }
