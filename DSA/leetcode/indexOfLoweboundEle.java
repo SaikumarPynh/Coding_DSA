8e11ad11e3d6b8e9f8c9e5eaa5d47fd192fe2e8d
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-04 09:38:57 +0530

    Floor in a Sorted Array adding comment statements

--- DSA/leetcode/indexOfLoweboundEle.java
+++ DSA/leetcode/indexOfLoweboundEle.java
@@ -1,4 +1,4 @@
-
+//Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
 public class indexOfLoweboundEle{
     
     // Function to find floor of x
