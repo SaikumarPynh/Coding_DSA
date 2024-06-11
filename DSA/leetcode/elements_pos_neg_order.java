e28c967630b917cbd2ad8b3ddc1be94f14415e4d
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-11 22:40:02 +0530

    java code for finding the element is pos or neg

--- DSA/leetcode/elements_pos_neg_order.java
+++ DSA/leetcode/elements_pos_neg_order.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 class solition{
    static  ArrayList<Integer> listp = new ArrayList<>();
     static ArrayList<Integer> listn = new ArrayList<>();
-
     public static void result(int[] arr){
         for(int i = 0;i<arr.length;i++){
             if(arr[i] < 0){
