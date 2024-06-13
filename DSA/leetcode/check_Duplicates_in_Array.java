12087431763b9aa95c7490ca073810979d4fe676
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-13 17:59:43 +0530

    checking whether the array contains duplicates or not using hashmap method

--- DSA/leetcode/check_Duplicates_in_Array.java
+++ DSA/leetcode/check_Duplicates_in_Array.java
@@ -26,5 +26,15 @@ class check_Duplicates_in_Array {
         //     }
         // }
         // return false;
+        // another efficient way using hashmap 
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i = 0;i<nums.length;i++){
+        //     //map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        //     if(map.containsKey(nums[i])){
+        //         return true;
+        //     }
+        //     map.put(nums[i],1);
+        // }
+        // return false;
     }
 }
