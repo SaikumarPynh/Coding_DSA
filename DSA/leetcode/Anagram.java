9eb7d9d1f64fd2f9cd872b0cd3093b9358a3f643
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-13 18:43:13 +0530

    checking the given two strings are anagram or not ,using hashmap method

--- DSA/leetcode/Anagram.java
+++ DSA/leetcode/Anagram.java
@@ -22,6 +22,23 @@ class annagram{
             System.out.print("not an anagrams");
 
         }
+        // HashMap<Character,Integer> map1 = new HashMap<>();
+        // HashMap<Character,Integer> map2 = new HashMap<>();
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // for(int i = 0;i<s.length();i++){
+        //     map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+        // }
+        // for(int i = 0;i<t.length();i++){
+        //     map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+        // }
+        // for(Character key:map1.keySet()){
+        //     if(!map1.get(key).equals(map2.get(key))){
+        //             return false;
+        //     }
+        // }
+        // return true;
 
     }
     public static void main(String[] args) {
