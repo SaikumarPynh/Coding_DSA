bcb41bfac3c728cb2595557479554d22345fb867
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-15 20:17:55 +0530

    You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).

--- DSA/leetcode/cont_with_most_water.java
+++ DSA/leetcode/cont_with_most_water.java
@@ -14,8 +14,21 @@ class Solution {
                 right--;
             }
         }
+        // int left = 0;
+        // int right = height.length - 1;
+        // int max = 0;
+        // while(left < right){
+        //     int min = Math.min(height[left],height[right]);
+        //     max = Math.max(max,min * (right - left));
+        //     if(height[left] < height[right]){
+        //         left++;
+        //     }else{
+        //         right--;
+        //     }
+        // }
         return max;
     }
+    
     public static void main(String[] args){
         Solution obj = new Solution();
         int[] height = {1,8,6,2,5,4,8,3,7};
