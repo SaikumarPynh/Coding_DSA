f4e2040874416bd4ad5f91c8855f95c71bd3aca8
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-15 22:53:44 +0530

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

--- DSA/leetcode/cont_with_most_water.java
+++ DSA/leetcode/cont_with_most_water.java
@@ -30,6 +30,8 @@ class Solution {
         // }
         return max;
     }
+
+    
     public static void main(String[] args){
         Solution obj = new Solution();
         int[] height = {1,8,6,2,5,4,8,3,7};
