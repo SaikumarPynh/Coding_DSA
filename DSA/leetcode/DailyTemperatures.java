315b5ef6e86886cb77d73abb7dcf25692231af12
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-16 15:25:32 +0530

    using stack approach

--- DSA/leetcode/DailyTemperatures.java
+++ DSA/leetcode/DailyTemperatures.java
@@ -4,16 +4,26 @@
 //  to wait after the ith day to get a warmer temperature. If there is no future day 
 //  for which this is possible, keep answer[i] == 0 instead.
 //https://leetcode.com/problems/daily-temperatures/
+import java.util.Stack;
 class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
+
         int[] arr = new int[temperatures.length];
-        for(int i = 0;i<temperatures.length - 1;i++){
-            for(int j = i+1;j<temperatures.length;j++){
-                if(temperatures[i] < temperatures[j]){
-                    arr[i] = j - i;
-                      break;
-                }
+        // for(int i = 0;i<temperatures.length - 1;i++){
+        //     for(int j = i+1;j<temperatures.length;j++){
+        //         if(temperatures[i] < temperatures[j]){
+        //             arr[i] = j - i;
+        //               break;
+        //         }
+        //     }
+        // }
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0;i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int idx = stack.pop();
+                arr[idx] = i - idx;
             }
+            stack.push(i);
         }
         return arr;
     }
