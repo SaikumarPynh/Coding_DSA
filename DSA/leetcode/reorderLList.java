b101115b850ffd85ef85a1ca03f342c5f05dfe71
SaikumarPynh <saikumarpynh@gmail.com>
2024-06-17 16:08:34 +0530

    Reordering the linked list  adding comment statements

--- DSA/leetcode/reorderLList.java
+++ DSA/leetcode/reorderLList.java
@@ -8,7 +8,14 @@ import java.util.Stack;
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+//  */
+// You are given the head of a singly linked-list. The list can be represented as:
+
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 class Solution {
     public void reorderLList(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
